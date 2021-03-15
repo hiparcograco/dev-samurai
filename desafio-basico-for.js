e4d0972b452e61b52e7a38b861b8d73c9c51dd3a
Hiparco <hiparco@gmail.com>
2021-03-15 04:18:20 -0300

    added desafio-for

---
+++ desafio-basico-for.js
@@ -0,0 +1,9 @@
+var inicial = 0;
+var final = 0;
+var incremento = 0;
+
+inicial = prompt ("Digite o valor inicial");
+final = prompt ("Digite o valor final");
+incremento = prompt ("Digite o valor incremento");
+
+alert ("Valor inicial: " + inicial + "\nvalor final:" + final + "\nvalor do incremento:" + incremento);
