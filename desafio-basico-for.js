d28a58cb4821733ecf34ce8252e118b62b30c825
Hiparco <hiparco@gmail.com>
2021-03-15 04:42:57 -0300

    corrigido desafio

--- desafio-basico-for.js
+++ desafio-basico-for.js
@@ -2,8 +2,14 @@ var inicial = 0;
 var final = 0;
 var incremento = 0;
 
-inicial = prompt ("Digite o valor inicial");
-final = prompt ("Digite o valor final");
-incremento = prompt ("Digite o valor incremento");
+inicial = Number(prompt ("Digite o valor inicial"));
+final = Number(prompt ("Digite o valor final"));
+incremento = Number(prompt ("Digite o valor incremento"));
 
 alert ("Valor inicial: " + inicial + "\nvalor final:" + final + "\nvalor do incremento:" + incremento);
+
+for (i=inicial;i<=final;i+=incremento){
+    console.log("Log("+i+") = " + Math.log(i));
+}
+
+
